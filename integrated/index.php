571c71600b918ad9015fef453c0e9cb943c74092
Irepan Salvador Martínez <irepan_salvador@hotmail.com>
2019-08-09 17:28:49 +0200

    Customise the buttons for upload files based on MPTP web

--- integrated/index.php
+++ integrated/index.php
@@ -7,7 +7,7 @@
 		<title>Cell lineage visualisation</title>
 		<!-- Custom CSS styles -->
 		<link href="style.css" rel="stylesheet" type="text/css" >
-        <link rel="stylesheet" href="./lib/d3-context-menu.css" />
+		<link rel="stylesheet" href="./lib/d3-context-menu.css" />
         <script src="./lib/d3.v3.js"></script>
         <script src="./lib/d3.v4.min.js"></script> <!-- 3d cells-->
         <script src="./lib/d3-3d.min.js"></script>
@@ -17,66 +17,87 @@
         <script src="./lib/d3-context-menu.js"></script>
         <script src="https://d3js.org/d3-color.v1.min.js"></script>
         <script src="https://d3js.org/d3-interpolate.v1.min.js"></script>
-        <script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
+				<script src="https://d3js.org/d3-scale-chromatic.v1.min.js"></script>
+    		<!-- Bootstrap -->
+    		<link href="./lib/bootstrap.min.css" rel="stylesheet">
     </head>
 
     <body>
 		<div id= "container", class="svg-container">
-			<h1> Cell lineage interactive visualisation</h1>
-            <h2> <i> by Irepan Salvador-Martinez </i> </h2>
+			<h3> Cell lineage interactive visualisation</h3>
+            <h4> <i> by Irepan Salvador-Martinez </i> </h4>
 
-            <!-- Custom Buttons -->
-            <input type="file" id="Tree_uploader">
+      <h6 class="pl-0">INPUT FILES</h6>
+      <div class="row">
+        <div class="col-sm-3 mb-2">
+          <a href="#" data-toggle="tooltip" data-placement="right" title="Input file containing a JSON tree with or without branch lengths."><label for="temp">Tree file:</label></a>
+          <div class="custom-file mb-3" id="temp">
+            <input type="file" class="custom-file-input" id="JSON_uploader" name="treefile">
+            <label class="custom-file-label" for="treefile">Input JSON tree file</label>
+          </div>
+				</div>
+		 
+				<div class="col-sm-3 mb-2">
+          <a href="#" data-toggle="tooltip" data-placement="right" title="Input file containing a Newick tree with or without branch lengths."><label for="temp">Tree file:</label></a>
+          <div class="custom-file mb-3" id="temp">
+            <input type="file" class="custom-file-input" id="Newick_uploader" name="treefile">
+            <label class="custom-file-label" for="treefile">or Input Newick tree file</label>
+          </div>
+				</div>
 
-            <button id="Reset" onclick="resetAll()">Reset Topology</button>
-            <button id="BranchLegths" onclick="show_bl()">BranchLen</button>
-            <button id="CollapseAll" onclick="collapseAll()">Collapse All</button>
-	        <button id="ExpandAll" onclick="expandAll()">Expand All</button>	
-             <!--<button id="Update" onclick="showmenu2()">Reset cells</button> -->
-            <!-- <button id="Disparity" onclick="disparity()">Disparity</button> -->
+				<div class="col-sm-3 mb-2">
+          <a href="#" data-toggle="tooltip" data-placement="left" title="Input file containing a Cell coordinates."><label for="temp">Coords file:</label></a>
+          <div class="custom-file mb-3" id="temp">
+            <input type="file" class="custom-file-input" id="3Dcoord_uploader" name="coords">
+            <label class="custom-file-label" for="treefile">Input coordinates file</label>
+          </div>
+				</div>
+      </div>
 
+          
+      <div class="status" align="right" > Click on a cell      </div>
             
-            <input type="file" id="3Dcoord_uploader">
-            
-            <div class="status" align="right" > Click on a cell      </div>
-            
-            <!-- Separate Areas -->
+     	<!-- Separate Areas -->
 			<div id="area1"> <!-- cell lineage -->
-            <h2>Cell lineage</h2>
+            <h4>Cell lineage</h4>
 			</div>
             
 			<div id="area2"> <!-- cells in 3d -->
-            <h2> Cells in 3D</h2>
+            <h4> Cells in 3D</h4>
             </div>
             <!-- Pan/zoom buttons  -->
             <div id= "controls_2" align="right" class="controls">
-                <button id="zoom_in" >+</button>
-                <button id="zoom_out">-</button>
-                <button id="pan_up"  >^</button>
-                <button id="pan_down">v</button>
-                <button id="pan_right">></button>
-                <button id="pan_left"><</button>
-                <button id="reset">Reset cols</button>
-                <input type="checkbox" id="Cells_checkbox"> Show Lineage rels   
-                <input type="number" id="CellSize" value=6>
+							<button id="zoom_in" >+</button>
+              <button id="zoom_out">-</button>
+              <button id="pan_up"  >^</button>
+              <button id="pan_down">v</button>
+              <button id="pan_right">></button>
+              <button id="pan_left"><</button>
+              <button id="reset">Reset cols</button>
+              <input type="checkbox" id="Cells_checkbox"> Show Lineage rels   
+              <input type="number" id="CellSize" value=6>
 
             </div>
         
             <div id= "controls_1" align="left" class="controls">
-                <button id="zoom_in_tree" >< ></button>
-                <button id="zoom_out_tree">> <</button>
-                <button id="pan_up_tree"  >^</button>
-                <button id="pan_down_tree">v</button>
-                <button id="Reset_cols_Tree">Reset cols</button>
-                <input type="checkbox" id="Tree_checkbox"> Show descendants   
-
+            	<button id="Reset" onclick="resetAll()">Reset Topology</button>
+            	<button id="BranchLegths" onclick="show_bl()">BranchLen</button>
+            	<button id="CollapseAll" onclick="collapseAll()">Collapse All</button>
+						</div>
+            <div id= "controls_1a" align="left" class="controls">
+							<button id="zoom_in_tree" >< ></button>
+              <button id="zoom_out_tree">> <</button>
+              <button id="pan_up_tree"  >^</button>
+              <button id="pan_down_tree">v</button>
+              <button id="Reset_cols_Tree">Reset cols</button>
+              <input type="checkbox" id="Tree_checkbox"> Show descendants   
             </div>
 		</div> 
         <div id="slider">
-           <h2>Tree depth</h2>
+           <h5>Tree depth</h5>
         </div>
         <div id="HM_scale">
-           <h2>Lineage relationships</h2>
+           <h5>Lineage relationships</h5>
         </div>
         
 
@@ -92,7 +113,7 @@
         <script src="brush_paryhale.js"></script>
         <script src="Nested_rels_scale.js"></script>
         <script>Coords_upload_button("3Dcoord_uploader", load_dataset_2)</script>
-        <script>Tree_upload_button("Tree_uploader", load_dataset_1)</script>
+        <script>Tree_upload_button("JSON_uploader", load_dataset_1)</script>
         <!-- Run functions to initialise the visualisation -->
         <script>
             init();
