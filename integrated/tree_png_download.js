3aef08c1eb4a3852a1210b12c84b90ab937366ab
Irepan Salvador Martínez <irepan_salvador@hotmail.com>
2020-10-05 09:05:03 +0200

    Add logo to tree downloaded image

--- integrated/tree_png_download.js
+++ integrated/tree_png_download.js
@@ -3,23 +3,36 @@ function SavePNG() {
 		console.log("Am I clicking here?");
 		var width = svg_tree.node().getBBox().width + 30;
 		var height = svg_tree.node().getBBox().height + 30;
+	
+		svgString =  addLogo(svg_tree,height)
+
 		if (height < 500) {height = 500}
 		if (width < 600) {width = 600}
+		
 		var svgString = getSVGString(svg_tree.node(), width, height);
 		svgString2Image( svgString,1.5*width,1.5*height, 'png', save ); // passes Blob and filesize String to the callback
 		function save( dataBlob, filesize ){
 				saveAs( dataBlob, 'cell_lineage.png' ); // FileSaver.js function
 		}
+	 svg_tree.select("#exportLogo").remove();
+
+
 	}
 function SaveSVG() {
 console.log("Am I clicking here?");
 	var width = svg_tree.node().getBBox().width + 30;
 	var height = svg_tree.node().getBBox().height + 30;
+	svgString =  addLogo(svg_tree,height)
+
+	
 	if (width < 600) {width = 600}
 	if (height < 500) {height = 500}
+	
 	var svgString = getSVGString(svg_tree.node(), width, height);
 	var blob = new Blob([svgString], {"type": "image/svg+xml;base64,"+ btoa(svgString)});
   saveAs(blob, "Cell_lineage.svg");
+  svg_tree.select("#exportLogo").remove();
+
 	}
 
 
@@ -125,4 +138,18 @@ function svgString2Image( svgString, width, height, format, callback ) {
 		});
 	};
 	image.src = imgsrc;
+}
+
+
+function addLogo(svg,height) {
+	var logo_xml = '<svg id="exportLogo" xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" version="1.1" viewBox="0 0 180 80" width = "180" height="80" x="-90" y="-90" > <g opacity=".5"> <path opacity="1" stroke-linejoin="round" fill="#808080" stroke="#fff" stroke-linecap="round" stroke-width=".75934" d="m132.59 93.069a3.4251 3.4251 0 0 1 -3.4242 3.4251 3.4251 3.4251 0 0 1 -3.426 -3.4233 3.4251 3.4251 0 0 1 3.4224 -3.4269 3.4251 3.4251 0 0 1 3.4278 3.4215"/> <g><g stroke-linejoin="round" fill-rule="evenodd" stroke="#fff" stroke-linecap="round" stroke-width=".75934"> <path opacity="1" fill="#808080" d="m129.64 97.152a3.4251 3.4251 0 0 1 -3.4242 3.4251 3.4251 3.4251 0 0 1 -3.426 -3.4233 3.4251 3.4251 0 0 1 3.4224 -3.4269 3.4251 3.4251 0 0 1 3.4278 3.4215"/> <path opacity="1" d="m136.04 95.426a3.4251 3.4251 0 0 1 -3.4242 3.4251 3.4251 3.4251 0 0 1 -3.426 -3.4233 3.4251 3.4251 0 0 1 3.4224 -3.4269 3.4251 3.4251 0 0 1 3.4278 3.4215"/> <path opacity="1" d="m133.05 99.913a3.4251 3.4251 0 0 1 -3.4242 3.4251 3.4251 3.4251 0 0 1 -3.426 -3.4233 3.4251 3.4251 0 0 1 3.4224 -3.4269 3.4251 3.4251 0 0 1 3.4278 3.4215"/></g><text style="word-spacing:0px;letter-spacing:0px" xml:space="preserve" font-size="15.875px" line-height="277.79998779%" y="102.27293" x="66.835686" font-family="Arial" fill="#000000"><tspan y="102.27293" x="66.835686" fill="#000000">CeLaVi</tspan></text> </g> <g stroke="#000" stroke-width=".75681" fill="none"><path d="m49.44 98.779h5.2917"/><path d="m54.732 93.728 0.000001 9.6212"/><path d="m54.732 103.35h10.583"/><path d="m54.732 93.728h5.2917"/><path d="m60.024 90.12v7.216"/><path d="m60.024 97.336h5.2917"/><path d="m60.024 90.12h5.291"/> </g></g> </g></svg>'
+	console.log("plotting height of " + height);
+	svg.append("g").html(logo_xml);
+	d3.select("#exportLogo")
+		.attr("width", "120px")
+		.attr("height", "100px")
+		.attr("x", "-70")
+		.attr("y", "-50")
+	d3.select("#area1").select("svg").selectAll("g").select("#exportLogo").raise();
+return svg;
 }
